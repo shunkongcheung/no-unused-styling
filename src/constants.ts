39ff74632b18b946157f46d893d8bb67826b83f0
Shun Kong Cheung <shcheung@microsoft.com>
2022-03-21 13:52:36 -0700

    feat(constants): types

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
+import { TSESTree } from "@typescript-eslint/typescript-estree";
+
+export type DeclareMap = Record<string, Array<TSESTree.Identifier>>;
+
 export const PATH_JOINNER = "_";
+
+export type UsageMap = Record<string, Array<string>>
 
 
